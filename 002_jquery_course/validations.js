144ddf1226dc62a66d21ba84419891d036ce914e
hortegag <hog_andy@hotmail.com>
2018-04-03 22:31:56 -0500

    002 jquery css selectors

---
+++ 002_jquery_course/validations.js
@@ -0,0 +1,11 @@
+$(function () {
+
+  alert('ready');
+  $('h1').text('Hello Duke');
+
+  $('h1.demo').text('Hello Spring');
+  $('.demo').text('Hello Jee');
+  $('#thirdHeader').text('Hello third header');
+
+}
+);
