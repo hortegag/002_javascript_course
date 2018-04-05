eb1e3c6f61983819e788bd455e4d05cfc6ef4060
hortegag <hog_andy@hotmail.com>
2018-04-05 01:14:36 -0500

    007 modifying content, register event and grab the element that raise the event

---
+++ 002_jquery_course/008_modifying_content/validations.js
@@ -0,0 +1,9 @@
+$(function () {
+
+  // use of $ before variable due to linter validations
+  $('#target').html('<p><u>updated with html content</u> </p>');
+
+  $('#display').text('<p><u>updated with text content</u> </p>');
+
+}
+);
